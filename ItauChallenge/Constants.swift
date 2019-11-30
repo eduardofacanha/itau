a246784bdab19a5992561b13890a3e424cd9e1df
Eduardo Façanha <eduardofacanha@MacBook-Pro-de-Eduardo.local>
2019-11-30 16:48:57 -0400

    IK1: Setup GoogleMapsAPI

---
+++ ItauChallenge/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  ItauChallenge
+//
+//  Created by Eduardo Façanha on 30/11/19.
+//  Copyright © 2019 Eduardo Façanha. All rights reserved.
+//
+
+import Foundation
+
+class Constants {
+  static let googlePlacesAPIKey = "AIzaSyA7RLMRwerruCRY3Vf--gNKJwy7QjvkZSg"
+}
